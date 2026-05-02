c22ec72a1b38b8f46143a2d86ad77aa92df11973
zeekoel <127354447+zeekoel@users.noreply.github.com>
2026-05-02 12:29:36 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.querySelector('#mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    mobileMenu.classList.toggle('is-active');
+});
